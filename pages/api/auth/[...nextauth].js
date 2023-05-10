e2cbad2593b9bcfb1a9fc2618a6f60f96f33545b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-10 11:54:04 +0600

    nextauth modifications

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
+import bcrypt from 'bcryptjs';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import { User } from '@/db/model/User';
 
 export const authOptions = {
   providers: [
@@ -33,23 +35,32 @@ export const authOptions = {
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
+      // credentials: {
+      //   email: { label: 'Email', type: 'email' },
+      //   password: { label: 'Password', type: 'password' },
+      // },
       async authorize(credentials, req) {
+        console.log('req....', req);
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' };
+        await databaseConnect();
+        const { email, password } = await credentials;
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
+        const user = await User.findOne({ email });
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        if (!user) {
+          throw new Error('Invalid Email or Password!');
         }
+        console.log('hola user ', user);
+        const isPasswordMatched = await bcrypt.compare(
+          password,
+          user?.password
+        );
+
+        if (!isPasswordMatched) {
+          throw new Error('Invalid Email or Password');
+        }
+
+        return user;
       },
     }),
   ],
